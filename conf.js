ad5d486b09fa3765d851b70c4229f10435a87dd1
Ankit Shankar <ankitshankar@gmail.com>
2011-11-04 22:58:49 -0400

    Rip out parts we don't want

---
+++ conf.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Facebook app
+    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || 'HAHABANANA',
+    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'HAHABANANA',
+    
+    session_secret: process.env.SESSION_SECRET || 'secret123',
+    port: process.env.PORT || 80,
+};
